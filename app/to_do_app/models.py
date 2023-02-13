6a4109e7afc21f24b85c056d8952deba2456cba9
Azamat Umurzakov <equal32296@gmail.com>
2023-02-14 03:53:36 +0600

    created class Task in models.py

--- app/to_do_app/models.py
+++ app/to_do_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 
+class Task(models.Model):
+    description = models.TextField(max_length=2000, blank=False, verbose_name="Описание")
+    status = models.CharField(max_length=200, blank=False, default="new", verbose_name="Статус")
+    complete_data = models.DateField(blank=True, default="", verbose_name="Дата выполнения")
+
+    def __str__(self):
+        return f"{self.description} {self.status}"
